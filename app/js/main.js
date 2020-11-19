71baa8cfaec1f124d728d9fd3e66bb30c566274e
Sergei Voloshenyuk <voloshenyuks@gmail.com>
2020-11-19 17:17:36 +0500

    added section top-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,11 @@
-$(function () {
+$(function (){
+
+  $('.top-slider__items').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
 });
