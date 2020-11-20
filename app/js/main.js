a0da7e84fa8269b770ebd1a7467c6a02eb08697e
Sergei Voloshenyuk <voloshenyuks@gmail.com>
2020-11-20 15:32:33 +0500

    added section info and catalog-week

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,14 @@ $(function (){
     autoplaySpeed: 2000
   });
 
+ $('.catalog__item').hover(
+    function() {
+      $( this ).addClass('catalog__item--active');
+    }, function() {
+      $( this ).removeClass('catalog__item--active');
+    }
+  );
+
+  let mixer = mixitup('.catalog__items');
+
 });
