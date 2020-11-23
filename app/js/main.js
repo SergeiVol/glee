1d630b6f2889820f402d26389f67ee5c91e1f4c3
Sergei Voloshenyuk <voloshenyuks@gmail.com>
2020-11-23 16:58:25 +0500

    fix section slider, order-info, catalog

--- app/js/main.js
+++ app/js/main.js
@@ -4,17 +4,9 @@ $(function (){
     dots: true,
     arrows: false,
     fade: true,
-    autoplay: true,
-    autoplaySpeed: 2000
+    // autoplay: true,
+    // autoplaySpeed: 2000
   });
-
- $('.catalog__item').hover(
-    function() {
-      $( this ).addClass('catalog__item--active');
-    }, function() {
-      $( this ).removeClass('catalog__item--active');
-    }
-  );
 
   let mixer = mixitup('.catalog__items');
 
