65897df1808871708307d3ebcbd246dc727d363a
Sergei Voloshenyuk <voloshenyuks@gmail.com>
2020-12-01 17:53:16 +0500

    fix mixer js

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,21 @@
-$(function (){
-
-  $('.top-slider__items').slick({
+$(function () {
+  $(".top-slider__items").slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
   });
 
-
-  let mixer = mixitup('.products-week__items', {
-      selectors: {
-          control: '.products-week__button'
-      }
+  let mixer = mixitup(".products-week__items", {
+    selectors: {
+      control: ".products-week__button",
+    },
   });
 
-  let mixer = mixitup('.new-design__items', {
-      selectors: {
-          control: '.new-design__button'
-      }
+  let mixer2 = mixitup(".new-design__items", {
+    selectors: {
+      control: ".new-design__button",
+    },
   });
-
 });
