6938aff90a94224684510596abf9e5424b2c22fc
Sergei Voloshenyuk <voloshenyuks@gmail.com>
2020-12-14 15:32:21 +0500

    added page Priduct list 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,21 @@
 $(function () {
+  $(".filter-recent__star").rateYo({
+    starWidth: "11px",
+    ratedFill: "#ffcc00",
+  });
+
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    onStart: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    },
+    onChange: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    },
+  });
+
   $(".top-slider__items").slick({
     dots: true,
     arrows: false,
